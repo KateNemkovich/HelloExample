40fd90397ef90ae60e0f509b53bfd31b766fce50
Nice3point <nice3point@gmail.com>
2022-07-19 22:35:40 +0300

    Remove I will try

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,7 +2,6 @@ Console.WriteLine("want to say hello");
 Console.WriteLine("Hello World!");
 Console.WriteLine("Just say hello");
 Console.WriteLine("Ok");
-Console.WriteLine("I will try");
 Console.WriteLine("Hello Kate!");
 Console.WriteLine("Hello World!");
 Console.WriteLine("Just hello");
