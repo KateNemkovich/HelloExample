8bdfa29967b2d6f6c4f0674f85068da9f41285d5
Kate <nemkovich.kate@gmail.com>
2022-07-19 21:52:05 +0300

    Addition try

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,5 +2,6 @@ Console.WriteLine("Don't want to say hello");
 Console.WriteLine("Hello World!");
 Console.WriteLine("Just say hello");
 Console.WriteLine("Ok");
+Console.WriteLine("I will try");
 Console.WriteLine("Hello, Git!");
  
