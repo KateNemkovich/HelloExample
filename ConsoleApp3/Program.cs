3b34ff7275260ea9bf58134a88c277e8e0b756aa
Nice3point <nice3point@gmail.com>
2022-07-19 21:50:25 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,5 +1,8 @@
-Console.WriteLine("Don't want to say hello");
+Console.WriteLine("want to say hello");
 Console.WriteLine("Hello World!");
-Console.WriteLine("Just say hello");
+Console.WriteLine("Hello World!");
+Console.WriteLine("Just hello");
 Console.WriteLine("Hello, Git!");
- 
+Console.WriteLine("Hello, Git!");
+Console.WriteLine("Hello, Git!");
+Console.WriteLine("Hello, Git!");
